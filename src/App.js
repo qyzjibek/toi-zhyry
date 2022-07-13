2d855da79dbef610ab04703353bd12dec9c72cde
Zhibek <zhibekrahym05@gmail.com>
2022-07-13 09:10:57 +0600

    feat: add music

--- src/App.js
+++ src/App.js
@@ -6,8 +6,25 @@ import { useState } from 'react';
 import {PricingTable} from './components/pricing-table/index';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Page1 } from './components/pages/page1';
+import { useEffect } from 'react';
+import Zhar from './assets/zhar-zhar.mp3'
 
 function App() {
+  let audio = new Audio(Zhar);
+
+  const start = () => {
+      audio.play();
+  }
+
+  const end = () => {
+    audio.pause();
+  }
+
+  useEffect(() => {
+    console.log("i am here");
+    start();
+}, []);
+
   const [total, setTotal] = useState(0);
 
   const handleExpenseChange = (sum) => {
@@ -17,21 +34,21 @@ function App() {
   return (
     <BrowserRouter>
       <ExpenseContext.Provider value={{handleExpenseChange, total}}>
-        <Header/>
+        <Header />
 
       <Routes>
         <Route
           path="/"
           element={
             <>
-              <div className="desc-wrapper">
+              <div className="desc-wrapper" >
               <div className="stats-wrapper">
                 <div className="stats">
                   <div className="stats-emoji">📊</div>
                   <div className="stats-text">Порядка 9,5% в объеме потребительского кредитования в Казахстане приходится на займы для проведения свадеб, сообщил председатель Нацбанка Казахстана Ерболат Досаев.</div>
                 </div>
               </div>
-              <Link to="/page1"><button id="calc-btn" style={{padding: "20px 40px", margin: "40px"}}>Посчитать расход моего тоя</button></Link>
+              <Link to="/page1"><button id="calc-btn" style={{padding: "20px 40px", margin: "40px"}} onClick={end}>Посчитать расход моего тоя</button></Link>
               </div>
             </>
           }
